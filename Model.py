35756d976095a2bab7ccf2b2351df9e96690d1b3
ShyGuyPy <lvawter@icprb.org>
2019-05-20 15:24:36 -0400

    removed duplicate import statements, variable declarations

--- Model.py
+++ Model.py
@@ -7,15 +7,6 @@ from flask_sqlalchemy import SQLAlchemy
 ma = Marshmallow()
 db = SQLAlchemy()
 
-from flask import Flask
-from marshmallow import Schema, fields, pre_load, validate
-from flask_marshmallow import Marshmallow
-from flask_sqlalchemy import SQLAlchemy
-
-
-ma = Marshmallow()
-db = SQLAlchemy()
-
 
 class Comment(db.Model):
     __tablename__ = 'comments'
